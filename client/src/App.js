13e7d164408f049861438fdcfe18bf4672b1510e
github username <github email address>
2018-05-13 20:39:22 -0400

    added jeopardy logo

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import JeopardyLogo from './components/JeopardyLogo';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <JeopardyLogo />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/JeopardyLogo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Header } from 'semantic-ui-react';
+
+const JeopardyLogo = () => {
+  return (
+    <Container text>
+      <Header 
+        as='h2'
+      >
+        JEOPARDY
+      </Header>
+    </Container>
+  )
+}
+
+export default JeopardyLogo;
