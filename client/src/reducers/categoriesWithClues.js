f545cadbd3a2610aa7489721eebf5bb7a8aa1313
brettCole <acodercalledcole@gmail.com>
2018-05-23 09:32:33 -0400

    removed loading and extended loading reducer

--- client/src/reducers/categoriesWithClues.js
+++ client/src/reducers/categoriesWithClues.js
@@ -1,9 +1,7 @@
 export default function fetchedCategoriesWithClues(state = [], action) {
-  // debugger;
   switch(action.type) {
     case 'FETCHED_CATEGORIES_WITH_CLUES':
       return {
-        loading: false,
         categoriesWithClues: action.payload
       }
     default:
--- client/src/reducers/loadingNotLoading.js
+++ client/src/reducers/loadingNotLoading.js
@@ -1,8 +1,12 @@
 export default function loadingNotLoading(state = { loading: false }, action) {
   switch(action.type) {
-    case 'CATEGORIES_WITH_CLUES_FETCH_STARTED':
+    case 'CATEGORIES_WITH_CLUES_FETCH_LOADING':
       return {
         loading: true
+      }
+    case 'CATEGORIES_WITH_CLUES_FINISHED_FETCH':
+      return {
+        loading: false
       }
     default:
       return state; 
