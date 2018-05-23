eefdfc859480f9c057ba638e3158eb1e92fe4463
brettCole <acodercalledcole@gmail.com>
2018-05-23 09:33:15 -0400

    created map function to render titles and removed static layout

--- client/src/containers/CategoryHeader.js
+++ client/src/containers/CategoryHeader.js
@@ -1,33 +1,44 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Grid } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 import CategoryAndValues from '../components/CategoryAndValues';
 
-export default class CategoryHeader extends Component {
+class CategoryHeader extends Component {
 
   render() {
+
+    const categories = ['category_one', 'category_two', 'category_three', 'category_four', 'category_five', 'category_six'];
+// debugger;
     return (
-      <Grid columns={6} divided container>
-        {/* <Grid.Row color='blue'> */}
-          <Grid.Column color='blue'>
-            <CategoryAndValues category={this.props.categoryOne} />
-          </Grid.Column>
-          <Grid.Column color='blue'>
-            <CategoryAndValues category={this.props.categoryTwo} />
-          </Grid.Column>
-          <Grid.Column color='blue'>
-            <CategoryAndValues category={this.props.categoryThree} />
-          </Grid.Column>
-          <Grid.Column color='blue'>
-            <CategoryAndValues category={this.props.categoryFour} />
-          </Grid.Column>
-          <Grid.Column color='blue'>
-            <CategoryAndValues category={this.props.categoryFive} />
-          </Grid.Column>
-          <Grid.Column color='blue'>
-            <CategoryAndValues category={this.props.categorySix} />
-          </Grid.Column>
-        {/* </Grid.Row> */}
-      </Grid>
+      <Grid  columns={6} divided container>
+        {
+          this.props.categorieTitles &&
+            categories.map((category, i) => {
+              return (
+                <Grid.Column 
+                  key={i} 
+                  color='blue'
+                >
+                  <CategoryAndValues
+                    key={i} 
+                    categoryTitle={ this.props.categorieTitles[category] }
+                  />
+                </Grid.Column>
+              )
+            })
+        }
+      </Grid> 
     )
   }
 }
+
+const mapStateToProps = state => {
+  return { categorieTitles: state.categoriesWithClues.categoriesWithClues }
+}
+
+CategoryHeader.propTypes = {
+  categoryTitles: PropTypes.object.isRequired
+}
+
+export default connect(mapStateToProps)(CategoryHeader);
