76143f4a9da61b342e8382c885ebcba49638e91e
brettCole <acodercalledcole@gmail.com>
2018-05-23 09:28:48 -0400

    added finish loading dispatch

--- client/src/actions.js
+++ client/src/actions.js
@@ -1,16 +1,18 @@
 export function receiveCategoriesWithClues(data) {
-  // debugger;
   return dispatch => {
     dispatch({
       type: 'FETCHED_CATEGORIES_WITH_CLUES',
       payload: data
+    });
+    dispatch({
+      type: 'CATEGORIES_WITH_CLUES_FINISHED_FETCH'
     });
   }
 }
 
 export function fetchCategoriesWithClues() {
   return dispatch => {
-    dispatch({ type: 'CATEGORIES_WITH_CLUES_FETCH_STARTED' });
+    dispatch({ type: 'CATEGORIES_WITH_CLUES_FETCH_LOADING' });
     return fetch('http://localhost:3001/api/v1/category.json', {
       method: 'GET',
       headers: {
