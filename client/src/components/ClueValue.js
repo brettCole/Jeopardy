6ba415cf3dc971fdf96861bf08a20872f4a4a7be
brettCole <acodercalledcole@gmail.com>
2018-06-03 22:51:15 -0400

    created initial point value board layout

---
+++ client/src/components/ClueValue.js
@@ -0,0 +1,240 @@
+import React, { Component } from 'react';
+import { Table, Item } from 'semantic-ui-react';
+
+class ClueValue extends Component {
+
+
+  test = (e) => {
+    debugger;
+  }
+
+  render() {
+
+    return (
+        <Table.Body>
+      <Table.Row>
+        <Table.Cell selectable onClick={this.test}>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>200</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>200</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>200</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>200</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>200</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>200</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>400</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>400</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>400</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>400</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>400</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>400</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>600</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>600</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>600</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>600</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>600</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>600</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>800</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>800</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>800</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>800</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>800</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>800</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell selectable onClick={this.test}>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>1000</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>1000</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>1000</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>1000</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>1000</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+        <Table.Cell selectable>
+          <Item>
+            <Item.Content>
+              <Item.Header as='h2'>1000</Item.Header>
+            </Item.Content>
+          </Item>
+        </Table.Cell>
+      </Table.Row>
+      </Table.Body>
+    )
+  }
+}
+
+export default ClueValue;
