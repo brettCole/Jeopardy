038afbb916afb4d89f5019361e887bf06c726290
brettCole <acodercalledcole@gmail.com>
2018-06-03 22:51:55 -0400

    changed from Grid to Table layout

--- client/src/containers/CategoryHeader.js
+++ client/src/containers/CategoryHeader.js
@@ -1,40 +1,40 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Grid } from 'semantic-ui-react';
+import { Table } from 'semantic-ui-react';
 import { connect } from 'react-redux';
-import CategoryAndValues from '../components/CategoryAndValues';
+import CategoryTitleAndClues from '../components/CategoryTitleAndClues';
 
 class CategoryHeader extends Component {
 
   render() {
 
     const categories = ['category_one', 'category_two', 'category_three', 'category_four', 'category_five', 'category_six'];
-// debugger;
+
     return (
-      <Grid  columns={6} divided container>
-        {
-          this.props.categorieTitles &&
-            categories.map((category, i) => {
-              return (
-                <Grid.Column 
-                  key={i} 
-                  color='blue'
-                >
-                  <CategoryAndValues
-                    key={i} 
-                    categoryTitle={ this.props.categorieTitles[category] }
-                  />
-                </Grid.Column>
-              )
-            })
-        }
-      </Grid> 
+      <Table.Header>
+        <Table.Row> 
+          {
+            this.props.categoryTitles && 
+              categories.map((category, i) => {
+                return (
+                  <Table.HeaderCell 
+                    key={i}
+                  >
+                    <CategoryTitleAndClues>
+                      {this.props.categoryTitles[category].options.title}
+                    </CategoryTitleAndClues>
+                  </Table.HeaderCell>
+                )
+              })           
+          }
+        </Table.Row> 
+      </Table.Header>
     )
   }
 }
 
 const mapStateToProps = state => {
-  return { categorieTitles: state.categoriesWithClues.categoriesWithClues }
+  return { categoryTitles: state.categoriesWithClues.categoriesWithClues }
 }
 
 CategoryHeader.propTypes = {
