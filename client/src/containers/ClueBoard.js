31cf9ad17c66cb0bccc231e1e05c9839456ae2c2
brettCole <acodercalledcole@gmail.com>
2018-06-19 20:18:25 -0400

    moved modalOpen to redux

--- client/src/containers/ClueBoard.js
+++ client/src/containers/ClueBoard.js
@@ -1,104 +1,125 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Header, Modal, Table } from 'semantic-ui-react';
-import ClueValue from '../components/ClueValue';
+import QuestionModalClue from '../components/QuestionModalClue';
 import CategorieTitleAndClues from '../components/CategoryTitleAndClues';
+import { bindActionCreators } from 'redux';
+import { modalOpenClick } from '../actions';
+
+// pass open state through to QuestionModalClue component
+// pass clue through to QuestionModlaClue component
 
 class ClueBoard extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {modalOpen: true};
+
+  handleOnClick = (e) => {
+    debugger;
+    this.props.modalOpenClick(e);
   }
 
   render() {
-
     const arr = [0, 1, 2, 3, 4, 5];
-// debugger;
     return (
-<React.Fragment>
-      <Modal open={this.state.modalOpen} centered={true} size='fullscreen' dimmer='blurring'
-      style={{'text-align': 'center', marginTop: '20px', 'background-color': '#00B1E1'}}>
-        <Modal.Content>
-          <Modal.Header>Official testing of Modal</Modal.Header>
-          <Modal.Description>
-            <Header>Here is some more testing script</Header>
-            <p>Well what do you think about all this text I'm throwing in here</p>
-            <p>Seems a little crazy right Idk well we will see what it looks like in the end.</p>
-          </Modal.Description>
-        </Modal.Content>
-      </Modal>
-
-
-      <Table.Body>
-        <Table.Row>
-          {
-            this.props.categorieClues &&
-              arr.map((ind, i) => {
-                return (
-                  <Table.Cell selectable>
-                    <CategorieTitleAndClues>200</CategorieTitleAndClues>
-                  </Table.Cell> 
-                )
-              })
-          }
-        </Table.Row>
-        <Table.Row>
-          {
-            this.props.categorieClues &&
-              arr.map((ind, i) => {
-                return (
-                  <Table.Cell selectable>
-                    <CategorieTitleAndClues>400</CategorieTitleAndClues>
-                  </Table.Cell>
-                )
-              })
-          }
-        </Table.Row>
-        <Table.Row>
-          {
-            this.props.categorieClues &&
-              arr.map((ind, i) => {
-                return (
-                  <Table.Cell selectable>
-                    <CategorieTitleAndClues>600</CategorieTitleAndClues>
-                  </Table.Cell>
-                )
-              })
-          }
-        </Table.Row>
-        <Table.Row>
-          {
-            this.props.categorieClues &&
-              arr.map((ind, i) => {
-                return (
-                  <Table.Cell selectable>
-                    <CategorieTitleAndClues>800</CategorieTitleAndClues>
-                  </Table.Cell>
-                )
-              })
-          }
-        </Table.Row>
-        <Table.Row>
-          {
-            this.props.categorieClues &&
-              arr.map((ind, i) => {
-                return (
-                  <Table.Cell selectable>
-                    <CategorieTitleAndClues>1000</CategorieTitleAndClues>
-                  </Table.Cell>
-                )
-              })
-          }
-        </Table.Row>
-      </Table.Body>
-
+      <React.Fragment>
+        <QuestionModalClue 
+          modalOpen = {this.props.modalOpen}
+          clue = {this.props.categoryClues}
+        />
+        <Table.Body as='tbody'>
+          <Table.Row as='tr' cellAs='td'>
+            {
+              this.props.categoryClues &&
+                arr.map((ind, i) => {
+                  return (
+                    <Table.Cell 
+                      selectable 
+                      onClick={this.handleOnClick}
+                      key={i}  
+                    >
+                      <CategorieTitleAndClues>200</CategorieTitleAndClues>
+                    </Table.Cell> 
+                  )
+                })
+            }
+          </Table.Row>
+          <Table.Row as='tr' cellAs='td'>
+            {
+              this.props.categoryClues &&
+                arr.map((ind, i) => {
+                  return (
+                    <Table.Cell 
+                      selectable
+                      onClick={this.handleOnClick}
+                      key={i}
+                    >
+                      <CategorieTitleAndClues>400</CategorieTitleAndClues>
+                    </Table.Cell>
+                  )
+                })
+            }
+          </Table.Row>
+          <Table.Row as='tr' cellAs='td'>
+            {
+              this.props.categoryClues &&
+                arr.map((ind, i) => {
+                  return (
+                    <Table.Cell 
+                      selectable
+                      onClick={this.handleOnClick}
+                      key={i}  
+                    >
+                      <CategorieTitleAndClues>600</CategorieTitleAndClues>
+                    </Table.Cell>
+                  )
+                })
+            }
+          </Table.Row>
+          <Table.Row as='tr' cellAs='td'>
+            {
+              this.props.categoryClues &&
+                arr.map((ind, i) => {
+                  return (
+                    <Table.Cell 
+                      selectable
+                      onClick={this.handleOnClick}
+                      key={i}  
+                    >
+                      <CategorieTitleAndClues>800</CategorieTitleAndClues>
+                    </Table.Cell>
+                  )
+                })
+            }
+          </Table.Row>
+          <Table.Row as='tr' cellAs='td'>
+            {
+              this.props.categoryClues &&
+                arr.map((ind, i) => {
+                  return (
+                    <Table.Cell 
+                      selectable
+                      onClick={this.handleOnClick}
+                      key={i}  
+                    >
+                      <CategorieTitleAndClues>1000</CategorieTitleAndClues>
+                    </Table.Cell>
+                  )
+                })
+            }
+          </Table.Row>
+        </Table.Body>
       </React.Fragment>
     )
   }
 }
   
 const mapStateToProps = state => {
-  return { categorieClues: state.categoriesWithClues.categoriesWithClues }
+  return { categoryClues: state.categoriesWithClues.categoriesWithClues,
+    modalOpen: state.modalOpenClose.modalOpen }
 }
 
-export default connect(mapStateToProps)(ClueBoard);
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ 
+    modalOpenClick, 
+  }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ClueBoard);
