039db0f23e29960761e6bfcf6ae4130c19b4e45a
brettCole <acodercalledcole@gmail.com>
2018-06-30 12:05:48 -0400

    included responsiveVoice.js

--- client/src/containers/ClueBoard.js
+++ client/src/containers/ClueBoard.js
@@ -1,29 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Header, Modal, Table } from 'semantic-ui-react';
+import { Table } from 'semantic-ui-react';
 import QuestionModalClue from '../components/QuestionModalClue';
 import CategorieTitleAndClues from '../components/CategoryTitleAndClues';
 import { bindActionCreators } from 'redux';
-import { modalOpenClick } from '../actions';
-
-// pass open state through to QuestionModalClue component
-// pass clue through to QuestionModlaClue component
+import { modalOpenClick, displayClue } from '../actions';
 
 class ClueBoard extends Component {
+  constructor(props) {
+    super(props);
 
-  handleOnClick = (e) => {
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
+  handleOnClick(e) {
+    this.props.modalOpenClick();
+    let newTitle = e.currentTarget.parentElement.offsetParent.children[0].childNodes[0].cells[e.currentTarget.cellIndex].innerText;
+    let categoryArray = ['category_one', 'category_two', 'category_three', 'category_four', 'category_five', 'category_six'];
+    
+    let clueNumber = Math.floor(Math.random() * this.props.categoryClues[categoryArray[e.currentTarget.cellIndex]].options.clues.length) + 1
+
+    let clueInfo = this.props.categoryClues[categoryArray[e.currentTarget.cellIndex]].options.clues[clueNumber].options
+    
+    let testing = window.responsiveVoice.speak("A second beast, it being like a bear. And on one side it was raised up, and there were three ribs in its mouth between its teeth; and this is what they were saying to it, Get up, eat much flesh.", "US English Male");
+
     debugger;
-    this.props.modalOpenClick(e);
+    this.props.displayClue(e);
+    // this.props.removeClueFromAllClues();
   }
 
   render() {
     const arr = [0, 1, 2, 3, 4, 5];
     return (
       <React.Fragment>
-        <QuestionModalClue 
+        {/* <QuestionModalClue 
           modalOpen = {this.props.modalOpen}
           clue = {this.props.categoryClues}
-        />
+        /> */}
         <Table.Body as='tbody'>
           <Table.Row as='tr' cellAs='td'>
             {
@@ -33,7 +46,8 @@ class ClueBoard extends Component {
                     <Table.Cell 
                       selectable 
                       onClick={this.handleOnClick}
-                      key={i}  
+                      key={i}
+                      disabled={false}
                     >
                       <CategorieTitleAndClues>200</CategorieTitleAndClues>
                     </Table.Cell> 
@@ -112,13 +126,16 @@ class ClueBoard extends Component {
 }
   
 const mapStateToProps = state => {
-  return { categoryClues: state.categoriesWithClues.categoriesWithClues,
-    modalOpen: state.modalOpenClose.modalOpen }
+  return { 
+    categoryClues: state.categoriesWithClues.categoriesWithClues,
+    modalOpen: state.modalOpenClick.modalOpen 
+  }
 }
 
 const mapDispatchToProps = dispatch => {
   return bindActionCreators({ 
-    modalOpenClick, 
+    modalOpenClick,
+    displayClue
   }, dispatch);
 }
 
