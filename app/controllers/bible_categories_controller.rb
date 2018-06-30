2d26dd213b833a5bc60de173e450e045455c782c
brettCole <acodercalledcole@gmail.com>
2018-06-30 11:59:40 -0400

    created index and create

--- app/controllers/bible_categories_controller.rb
+++ app/controllers/bible_categories_controller.rb
@@ -1,12 +1,15 @@
 class BibleCategoriesController < ApplicationController
 
   def index
-
+    categories = BibleCategory.all
+    render json: categories, include: [:bible_clues], status: 200
   end
 
   def create
-    bible_category = BibleCategories.new(bible_category_params)
-    if bible_category.valid? && bible_category.save
+    bible_category = BibleCategory.new(bible_category_params)
+    if bible_category.valid? && bible_category.save 
+      render status: 200
+    end
   end
 
   private
