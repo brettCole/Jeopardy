74db13e947392ced69382917de3509617eb77a39
brettCole <acodercalledcole@gmail.com>
2018-07-08 11:04:43 -0400

    made dynamic players instead of hard coded

--- client/src/components/ModalPlayerButtons.js
+++ client/src/components/ModalPlayerButtons.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 
 const ModalPlayerButtons = (props) => {
+  const players = () => {
+    let finalPlayers = [];
+    for (let i = 0; i < 3; i++) {
+      finalPlayers.push(
+        <Button key={i} size="massive" onClick={props.playersAnswer}>Player {i + 1}</Button>
+      )
+    }
+    return finalPlayers
+  }
+  
+
   return (
     <Button.Group color='blue' as='h2'>
-      <Button size="massive">Player 1</Button>
-      <Button size="massive">Player 2</Button>
-      <Button size="massive">Player 3</Button>
+      {players()}
     </Button.Group>
   )
 }
 
-export default ModalPlayerButtons;
+export default connect()(ModalPlayerButtons);
