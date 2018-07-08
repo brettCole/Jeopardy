64f07a732ae4ab8c5a48b0514def3e70a5b41a91
brettCole <acodercalledcole@gmail.com>
2018-07-08 11:02:02 -0400

    added clue and form for answer to clue with clickable players

--- client/src/components/QuestionModalClue.js
+++ client/src/components/QuestionModalClue.js
@@ -1,15 +1,71 @@
-import React from 'react';
-import { Modal } from 'semantic-ui-react';
+import React, { Fragment, Component } from 'react';
+import { Form, Modal } from 'semantic-ui-react';
+import ModalPlayerButtons from './ModalPlayerButtons';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { currentPlayer } from '../actions';
 
-const QuestionModalClue = (props) => {
-  return (
-    <Modal open={props.modalOpen} centered='true' size='fullscreen' dimmer='blurring'
-    style={{'textAlign': 'center', marginTop: '20px', 'backgroundColor': '#00B1E1'}}>
-      <Modal.Content>
-        <Modal.Header>[props.clue}</Modal.Header>
-      </Modal.Content>
-    </Modal>
-  )
+class QuestionModalClue extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      disabled: true
+    }
+    this.playersAnswer = this.playersAnswer.bind(this);
+  }
+
+  playersAnswer = (e) => {
+    let player = e.currentTarget.textContent;
+    this.props.currentPlayer(player);
+    this.setState({
+      disabled: false
+    })
+  }
+
+  render() {
+    return (
+      <Modal 
+        open={this.props.modalOpen}
+        centered={true}
+        size='fullscreen' 
+        dimmer='blurring'
+        style={{ 'textAlign': 'center', 'marginTop': '20px', 'backgroundColor': '#2185d0' }} 
+      >
+        <Modal.Content
+          style={{ 'backgroundColor': '#2185d0', 'color': '#fff' }}
+        >
+          {this.props.clue !== undefined &&
+            <Modal.Header
+              as='h1'
+              onClick={this.props.test}      
+            >
+              {this.props.clue.description}
+            </Modal.Header>
+          }
+        </Modal.Content>
+        <ModalPlayerButtons playersAnswer={this.playersAnswer} />
+      
+        <Form
+          as='form'
+          size='massive'
+        >
+          <Form.Field width={6} style={{ 'marginLeft': 'auto', 'marginRight': 'auto', 'marginTop': '20px', 'marginBottom': '10px' }}
+            disabled={this.state.disabled}
+          >
+            <label style={{'color': 'white'}}>Player's Answer</label>
+            <input placeholder='What is...' />
+          </Form.Field>
+        </Form>
+      </Modal>
+    )
+  }
 }
 
-export default QuestionModalClue;
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({
+    currentPlayer
+  }, dispatch);  
+}
+
+export default connect(null, mapDispatchToProps)(QuestionModalClue);
