8a2786942159665b0d9ac2043de30ff4f4b56280
brettCole <acodercalledcole@gmail.com>
2018-07-08 11:02:25 -0400

    added react router for routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-
-
-import GameBoard from './containers/GameBoard';
+import BibleBoard from './containers/BibleBoard';
+import GameBaord from './containers/GameBoard';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <GameBoard />
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path='/' component={GameBaord} />
+          <Route exact path='/bible_jeopardy' component={BibleBoard} />
+        </div>
+      </Router>
     );
   }
 }
