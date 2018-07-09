0b5592ecfb59a7d19d6d4f406d03405a4f0d4a97
brettCole <acodercalledcole@gmail.com>
2018-07-09 07:38:15 -0400

    green or red response based on correct answer or not

--- client/src/components/QuestionModalClue.js
+++ client/src/components/QuestionModalClue.js
@@ -1,5 +1,5 @@
 import React, { Fragment, Component } from 'react';
-import { Form, Modal } from 'semantic-ui-react';
+import { Form, Message, Modal } from 'semantic-ui-react';
 import ModalPlayerButtons from './ModalPlayerButtons';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -10,9 +10,36 @@ class QuestionModalClue extends Component {
     super(props);
 
     this.state = {
-      disabled: true
+      disabled: true,
+      hidden: true,
+      correct: false,
+      value: ''
     }
     this.playersAnswer = this.playersAnswer.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      value: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      hidden: false,
+      value: ''
+    })
+    if (this.state.value.toLowerCase() === this.props.clue.answer.toLowerCase()) {
+      this.setState({
+        correct: true
+      })
+    }
+    this.setState({
+      disabled: true
+    })
   }
 
   playersAnswer = (e) => {
@@ -49,12 +76,42 @@ class QuestionModalClue extends Component {
         <Form
           as='form'
           size='massive'
+          onSubmit={this.handleSubmit}
         >
           <Form.Field width={6} style={{ 'marginLeft': 'auto', 'marginRight': 'auto', 'marginTop': '20px', 'marginBottom': '10px' }}
             disabled={this.state.disabled}
           >
             <label style={{'color': 'white'}}>Player's Answer</label>
-            <input placeholder='What is...' />
+            <Form.Input 
+              type="text"
+              placeholder="What is..."
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </Form.Field>
+          <Form.Field
+            width={6}
+            style={{ 'marginLeft': 'auto', 'marginRight': 'auto', 'marginTop': '20px', 'marginBottom': '10px' }}
+          >
+          {
+            this.state.correct ? (
+              <Message
+                hidden={this.state.hidden}
+                size="massive"
+                color="green"
+                header="Correct Answer"
+                content="Current score plus 200"
+              />
+            ) : (
+              <Message
+                hidden={this.state.hidden}
+                size="massive"
+                color="red"
+                header="Incorrect Answer"
+                content="Current score minus 200"
+              />
+            )
+          }
           </Form.Field>
         </Form>
       </Modal>
