a5a97a97e671eeef8d17bf91d9ad573185bfe023
brettCole <acodercalledcole@gmail.com>
2018-07-14 21:51:12 -0400

    change general component to show multiple things

--- client/src/components/CategoryTitleAndClues.js
+++ client/src/components/CategoryTitleAndClues.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Item } from 'semantic-ui-react';
 
 const CategoryTitleAndClues = (props) => {
   return (
     <Item>
       <Item.Content>
-        <Item.Header as='h2'>{props.children}</Item.Header>
+        {
+          props.what ? (
+            <Fragment>
+              <Item.Header as='h1' style={{ 'display': 'inline', 'marginRight': '25%' }}>{props.children}</Item.Header>
+              <Item.Header as='h1' style={{ 'display': 'inline', 'marginLeft': '25%' }}>200</Item.Header>
+            </Fragment>
+          ) : (
+            <Item.Header as='h1'>
+              {props.children}
+            </Item.Header>
+          )
+        }
       </Item.Content>
     </Item>
   )
