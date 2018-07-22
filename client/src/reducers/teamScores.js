be4e795bd6a425d01b5c832c2a97e5f2a5e6422d
brettCole <acodercalledcole@gmail.com>
2018-07-21 23:29:02 -0400

    add score to team reducer

--- client/src/reducers/teamScores.js
+++ client/src/reducers/teamScores.js
@@ -4,6 +4,14 @@ export default function teamScores(state = [
   {'Team 3': 0}
 ], action) {
   switch(action.type) {
+    case 'ADD_TO_TEAMS_SCORE':
+      const updatedScore = state.map((each, i) => {
+        if (action.playerGuessing === Object.keys(each).toString()) {
+          return {[Object.keys(each).toString()]:parseInt(Object.values(each)) + action.point_value}
+        }
+          return each
+        })
+        return updatedScore
     default:
       return state;
   }
